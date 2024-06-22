3e134ea339f0cd7acf1d638b05b2c4fbda088e08
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-06-22 04:02:28 -0700

    renamed:    BMI CALCULATOR.py -> Day-2/BMI CALCULATOR.py 	renamed:    Life In weeks.py -> Day-2/Life In weeks.py 	renamed:    Tip_Calculator.py -> Day-2/Tip_Calculator.py

---
+++ Day-3/Conditional Statements.py
@@ -0,0 +1,7 @@
+height = int(input("Enter Your Height(in cm): "))
+
+if (height>=120):
+    print("You are allowed to Ride the Rollercoaster")
+else:
+    print("You are Forbidden From Riding the Rollercoaster !!!")
+
