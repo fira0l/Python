7cd9ef66ae99dd14d2520e1f8ac3fac5251cffc3
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-06-25 09:13:32 -0700

    Reeborgs code for day6

---
+++ Day-6/Functions.py
@@ -0,0 +1,48 @@
+"""
+THIS IS A CODE FOR REEBORG.CA
+"""
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+for i in range(1,7):
+   jump()
+ """  
+"""
+    while not at_goal:
+        jump()
+    #this an alternative for the the for loop above and is used to solve the hardle 2 
+    
+"""
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+        
+"""
