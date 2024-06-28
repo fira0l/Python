59070723a0ce00ee234d0c14c85eaf7852b24426
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-06-28 02:41:59 -0700

    Final Completed Hangman word guessing game using python enjoy it

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -1,6 +1,14 @@
 import random
+import hangman_Arts as arts
+import hangman_words as words
 
-word_list =["ardvark","baboon","camel"]
+print(arts.logo)
+
+
+word_list =words.word_list
+
+lives = 6 
+end_of_game = False
 
 display= []
 counter = 0
@@ -8,22 +16,40 @@ counter = 0
 # letter = input("guess a letter : ")
 
 chosen_word = random.choice(word_list)
-print(f"Psst, The choosen Word is {chosen_word}.")
+# print(f"Psst, The choosen Word is {chosen_word}.")
 
 for letter in chosen_word:
     display += "_"
 print(display)
 
 
-while ("_" in display):
+while not end_of_game:
     guess = input("Guess a Letter: ").lower()
+    
+    if guess in display:
+        print(f"You Have already guessed {guess} letter")
+    
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         # print(f"Current position: {position}\nCurrent letter: {letter}\nGuessed letter: {guess}")
         if letter == guess:
             display[position] = letter
     print(display)
-print("You win!!!")
+    
+    if guess not in chosen_word:
+        print(f"You Guessed {guess},that is not in the Word, You lose a life ")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You Lose.")
+            
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You Win.")
+        
+    print(arts.stages[lives])
+
     
     
 
