93cadccc6853ce0d009b84aa8b53b45e34eb247f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-06-29 23:41:52 -0700

    Final Ceaser Cypher completed Code

--- Day-8/CeaserCypher.py
+++ Day-8/CeaserCypher.py
@@ -1,22 +1,37 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(plain_text,shift_amount):
-    cipher_text=""
-    for letter in plain_text:
-      position = alphabet.index(letter)  
-      new_position = position + shift_amount
-      new_letter = alphabet[new_position]
-      cipher_text += new_letter
-    print(f"The Encoded text is {cipher_text}")
-      
-        
-    
-
+end_of_cipher = False
+while not end_of_cipher:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  # cont = input("Do you want to Continue: \"Yes\" or \"no\"")
+  
+  shift = shift % 26
+  
+  def ceaser(start_text,shift_amount,cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+      shift_amount *=-1
+    for char in start_text:
+      if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+      else:
+        end_text += char
+    print(f"The {cipher_direction}d text is:' {end_text} '")
+  ceaser(start_text=text,shift_amount=shift,cipher_direction=direction)
+  cont = input("Do you want to Continue: \"Yes\" or \"no\"").lower()
+  if cont == "no":
+    end_of_cipher = True
+    print("Good Bye") 
+  
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
     #plain_text = "hello"
@@ -29,6 +44,6 @@ def encrypt(plain_text,shift_amount):
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-encrypt(plain_text=text,shift_amount=shift)
+
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
