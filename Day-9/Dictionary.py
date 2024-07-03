464ee0c9bc5f37d16fab43831b15b6c9764b5f27
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-03 01:56:44 -0700

    dictionary commit

---
+++ Day-9/Dictionary.py
@@ -0,0 +1,7 @@
+programming_dictionary = {
+    "bug":"An error in a program that prevents the program from running as excpected",
+    "function":"A piece of code that you can easily call over and over again",
+    "loop":"The action of doing sommething over and over again"
+}
+
+print(programming_dictionary)
