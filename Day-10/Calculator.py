8f6200be573df1e425a9fe552431caba1f67643e
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-04 04:17:31 -0700

    The First Phase of Building a calculator

---
+++ Day-10/Calculator.py
@@ -0,0 +1,35 @@
+from art import logo
+print(logo)
+
+def add(n1,n2):
+    """Returns the Sum of Two Numbers provided in the parameters"""
+    return n1+n2
+def subtract(n1,n2):
+    """Returns the difference of the two numbers provided in the parameters"""
+    return n1 - n2
+def multiply(n1,n2):
+    """Returns the product of the two numbers provided in the parameters"""
+    return n1*n2
+def division(n1,n2):
+    """Returns the Division of the two numbers provided in the parameters"""
+    if n2 == 0:
+        return "Are u Dumb u cant divide a number with 0 change it!"
+    else:
+        return n1/n2
+    
+operations = {
+    '+':add,
+    '-':subtract,
+    '*':multiply,
+    '/':division
+}
+
+num1 = int(input("What is the first Number?: "))
+for keys in operations:
+    print(keys)
+operation_symbol = input("Pick an operaton from the line above: ")
+num2 = int(input("What is the second Number?: "))
+
+answer = operations[operation_symbol](num1,num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
