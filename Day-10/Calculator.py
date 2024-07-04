baa5ae316324c4702811004513d106eef29a60a1
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-04 04:28:57 -0700

    A slight Modification

--- Day-10/Calculator.py
+++ Day-10/Calculator.py
@@ -30,6 +30,13 @@ for keys in operations:
 operation_symbol = input("Pick an operaton from the line above: ")
 num2 = int(input("What is the second Number?: "))
 
-answer = operations[operation_symbol](num1,num2)
+first_answer = operations[operation_symbol](num1,num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next Number?: "))
+calculation_function= operations[operation_symbol]
+second_answer = calculation_function(calculation_function(num1,num2),num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
