c1fdcdfa564c6ed00c66e5922b748cdebe4eef6d
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-07 10:45:47 -0700

    Do u guys know that if u type #type:ignore on a line u will ignore a warning on that specific line?

--- Day-12/Scope.py
+++ Day-12/Scope.py
@@ -2,11 +2,11 @@
 
 def my_function():
     counter = 0;
-    if len(name)>0:
+    if len(name)>0: # type: ignore
         counter+=1
     print(counter)
     
-print(counter)
+print(counter) #type: ignore
 
 #This two counter prints are used to elaborate about Local scope
 #meaning that variables declared inside function they are only usable inside the function
