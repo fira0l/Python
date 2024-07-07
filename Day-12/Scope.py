5e5804cf816d402f933ccd091db9a2fbda6ffcde
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-07 10:38:10 -0700

    Init on scopes

---
+++ Day-12/Scope.py
@@ -0,0 +1,12 @@
+"""This  is created for the purpose exploring scopes"""
+
+def my_function():
+    counter = 0;
+    if len(name)>0:
+        counter+=1
+    print(counter)
+    
+print(counter)
+
+#This two counter prints are used to elaborate about Local scope
+#meaning that variables declared inside function they are only usable inside the function
