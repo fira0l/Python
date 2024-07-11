d98e036a5f846fdf01d2cb099e4d8b4aa9c7b95d
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-11 09:24:51 -0700

    scopes continued

--- Day-12/Scope.py
+++ Day-12/Scope.py
@@ -1,12 +1,24 @@
 """This  is created for the purpose exploring scopes"""
 
-def my_function():
-    counter = 0;
-    if len(name)>0: # type: ignore
-        counter+=1
-    print(counter)
+enemies = 1
+
+# def my_function():
+#     counter = 0;
+#     if len(name)>0: # type: ignore
+#         counter+=1
+#     print(counter)
     
-print(counter) #type: ignore
+# print(counter) #type: ignore
 
 #This two counter prints are used to elaborate about Local scope
 #meaning that variables declared inside function they are only usable inside the function
+
+def increase_enemies():
+    global enemies
+    enemies += 1
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+
+#This is Function declared to show how to use global variables inside a function
+
