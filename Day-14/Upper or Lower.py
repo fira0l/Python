e8c0740d32a51e30ce1e1af847ebff779f430582
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-23 07:41:49 -0700

    Creating a Function To Handle Account meaning Cards to Compare

--- Day-14/Upper or Lower.py
+++ Day-14/Upper or Lower.py
@@ -1,5 +1,7 @@
 #to do
 from arts import logo,vs
+from Dictionary import data
+import random
 #logo
 print(logo)
 #Landing Page
@@ -14,8 +16,24 @@ The data is based on global monthly searches.
 
 ## Courses Comments
 # Generate a random account from the game data.
-
+def Account(dt,choice):
+    """This Function is Used To Retrive and Assign Values To Respective Dictionary Values"""
+    account = random.choice(dt)
+# print(account)
 # Format account data into printable format.
+    name = account["name"]
+    follower_count = int(account["follower_count"])
+    description = account["description"]
+    country = account["country"]
+    
+    print(f"{choice} = {name}, {country}")
+    print(description)
+    
+a = Account(dt=data,choice="A")
+print(vs)
+b = Account(dt=data,choice="B")
+
+
 
 # Ask user for a guess.
 
