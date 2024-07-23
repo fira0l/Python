a563b931fa6b2aa3d646f15346a14e16cae677f1
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-23 06:10:10 -0700

    Initializing the Upper or Lower Game

---
+++ Day-14/Upper or Lower.py
@@ -0,0 +1,36 @@
+#to do
+from arts import logo,vs
+#logo
+print(logo)
+#Landing Page
+print("""What gets Googled more?
+A frustratingly addictive game of higher or lower using Google searches.
+The data is based on global monthly searches.
+""")
+#Handling User Input
+#creating a Function That Handles the comparision of values
+#Looping
+#game Status
+
+## Courses Comments
+# Generate a random account from the game data.
+
+# Format account data into printable format.
+
+# Ask user for a guess.
+
+# Check if user is correct.
+## Get follower count.
+## If Statement
+
+# Feedback.
+
+# Score Keeping.
+
+# Make game repeatable.
+
+# Make B become the next A.
+
+# Add art.
+
+# Clear screen between rounds.
