7e5072b54610bb8666563847660c975064c5c8bd
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-27 10:21:11 -0700

    Day-16 initial code about turtle

---
+++ Day-16/Object_oriented_programming.py
@@ -0,0 +1,15 @@
+# import turtle
+
+# mitty = turtle.Turtle()
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# mitty.shape("turtle")
+# mitty.color("blue","coral")
+
+
+# mitty.forward(100)
+
+
+# my_screen.exitonclick()
+
+
