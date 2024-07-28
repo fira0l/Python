61b249c6195ffa2b9759d4dff851ddd40366edb5
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-28 12:26:21 -0700

    Adding External package and use it

--- Day-16/Object_oriented_programming.py
+++ Day-16/Object_oriented_programming.py
@@ -1,4 +1,5 @@
 # import turtle
+#from prettytable import PrettyTable
 
 # mitty = turtle.Turtle()
 # my_screen = turtle.Screen()
@@ -12,4 +13,12 @@
 
 # my_screen.exitonclick()
 
+
+#import external package called prettytable from pypi.org
+
+#from prettytable import PrettyTable
+#table = PrettyTable()
+#table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+#table.add_column("Type",["Electric","Water","Fire"])
+#print(table)
 
