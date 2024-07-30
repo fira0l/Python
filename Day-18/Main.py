1b12125cc6fb635791b1bea4034a83992a7f4329
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-30 12:23:46 -0700

    IMported The Turtle library and used it

---
+++ Day-18/Main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("cyan","black")
+timmy_the_turtle.forward(250)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
