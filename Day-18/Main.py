f70f13fd19df62b0626575a23c8b2d19e7a97367
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-30 12:59:18 -0700

    Done it my way if it works it is ok. right?

--- Day-18/Main.py
+++ Day-18/Main.py
@@ -1,12 +1,26 @@
 from turtle import Turtle, Screen
-
+# import heroes
 tim = Turtle()
 tim.shape("turtle")
 tim.color("cyan", "black")
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
+for step in range(15):
+    for step in ("black","white"):
+        tim.pencolor(step)
+        tim.forward(10)
+tim.pencolor("black")
+tim.right(135)
+tim.forward(10)
+tim.right(180)
+tim.forward(10)
+tim.left(90)
+tim.forward(10)
+tim.penup()
+tim.goto(0,0)
+# print(heroes.gen())
 
 
 
