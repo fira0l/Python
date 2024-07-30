e059ebae40a5e9964fb19546d1001f2da1725a55
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-30 10:29:39 -0700

    Creating Methods in a class

--- Day-17/DAy17.py
+++ Day-17/DAy17.py
@@ -3,7 +3,18 @@ class User:
         print("New Users begin Created!!!")
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self,user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001","Firaol")
+user_2 = User("002","Fenet")
 print(user_1.username)
+
+user_1.follow(user_2)
+print(user_1.following)
+print(user_2.followers)
