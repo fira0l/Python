83d4e0105f6355e4ac6bc499593f4b48edcaf45a
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-30 03:01:14 -0700

    This Modified Coffe Machine using Obeject Oriented Programming

--- Day-16/CoffeMachineRefined.py
+++ Day-16/CoffeMachineRefined.py
@@ -4,22 +4,43 @@ from money_Machine import MoneyMachine
 
 
 menu = Menu()
-coffeeMaker = CoffeeMaker()
-payment = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
 
+coffee_maker.report()
+money_machine.report()
 is_on = True
 while is_on:
-    choice = input(f"​What would you like? {menu.get_items()}: ")
+    options = menu.get_items()
+    choice = input(f"What Would u Like? ({options}): ")
     if choice == "off":
         is_on = False
-    elif choice == "report":
-       coffeeMaker.report()
+    elif choice == ("report"):
+        coffee_maker.report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        if coffeeMaker.is_resource_sufficient(choice):
-            paymentt = payment.process_coins()
-            if payment.make_payment(paymentt):
-                coffeeMaker.make_coffee(choice)
+        # print(drink)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+# is_on = True
+# while is_on:
+#     choice = input(f"​What would you like? {menu.get_items()}: ")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#        coffeeMaker.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#         if coffeeMaker.is_resource_sufficient(choice):
+#             paymentt = payment.process_coins()
+#             if payment.make_payment(paymentt):
+#                 coffeeMaker.make_coffee(choice)
 
 
