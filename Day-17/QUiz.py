5065a678e8f08975be8653282c076685a14adc27
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-30 11:22:54 -0700

    Created A quiz brain class and made it accept Users input

--- Day-17/QUiz.py
+++ Day-17/QUiz.py
@@ -1,6 +1,6 @@
 from data import question_data
 from question_model import Question
-
+from quiz_brain import QuizBrain
 
 question_bank = []
 for questions in question_data:
@@ -9,6 +9,8 @@ for questions in question_data:
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 #     for keys in questions:
 #         question = Question(keys,questions[keys])
 #         question_bank.append(question)
