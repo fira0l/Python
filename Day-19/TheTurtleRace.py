95757b54007a25518413e21e9d285647cd0e7369
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-07-31 11:12:42 -0700

    Awesome Game called Turtle Race Enjoy it

---
+++ Day-19/TheTurtleRace.py
@@ -0,0 +1,35 @@
+import random
+import turtle
+from turtle import Turtle, Screen
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make Your Bet", prompt="Which Turtle will win the race? Enter a Color: ")
+color = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've Won. the {winning_color} turtle is the winner!")
+            else:
+                print(f"You Have Lost. the {winning_color} turtle Is the Winner!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+screen.exitonclick()
