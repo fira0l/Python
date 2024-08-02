c080cafc7e735755698ba1fb1054d67edff99d95
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 23:03:11 -0700

    used the Trace and Update modules of this Game

--- Day-20/main.py
+++ Day-20/main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle,Screen
 
 screen = Screen()
@@ -5,6 +6,7 @@ screen = Screen()
 screen.title("The Snake Game")
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -13,10 +15,19 @@ segments = []
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
     segments.append(new_segment)
 
 
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
 
 
 
