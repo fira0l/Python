19b7c0a8159088caf6c4ce1145de325133a45a78
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-02 02:51:53 -0700

    Created The Snake Class and migrated the Required Code to the snake Class

--- Day-20/main.py
+++ Day-20/main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Turtle,Screen
+from snake import Snake
 
 screen = Screen()
 
@@ -8,18 +9,9 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
 
 # Created The snake Here
-
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 
 game_is_on = True
@@ -27,12 +19,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-# Made The Snake Segments to Follow Each other
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+
+    snake.move()
+
 
 
 
