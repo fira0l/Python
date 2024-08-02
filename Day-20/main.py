fd187ae6d20a4e0ee31cc19da7a0774cc1deb2c0
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 23:20:52 -0700

    Making the segments to follow eachother

--- Day-20/main.py
+++ Day-20/main.py
@@ -9,7 +9,6 @@ screen.bgcolor("black")
 screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
 segments = []
 
 for position in starting_positions:
@@ -26,8 +25,13 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
 
 
 
