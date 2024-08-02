8b24a548776db0e6dbc554d349dbf29f1b6abd59
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 22:53:11 -0700

    modified the segment using the for loop

--- Day-20/main.py
+++ Day-20/main.py
@@ -6,18 +6,14 @@ screen.title("The Snake Game")
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 
-starting_positions = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segment_1 = Turtle(shape="square")
-segment_1.color("white")
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
-segment_2 = Turtle(shape="square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
 
-segment_3 = Turtle(shape="square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
 
 
 
