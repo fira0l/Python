c1995d5ddd65c80f32f8d9922f81f1038e00f6a1
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-02 03:44:16 -0700

    created the control logic for the snake now the user can controll the snake

--- Day-20/snake.py
+++ Day-20/snake.py
@@ -2,21 +2,39 @@ from turtle import Turtle
 
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
+MOVE_DISTANCE = 20
 
 class Snake:
-    for position in starting_positions:
-        new_segment = Turtle(shape="square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        segments.append(new_segment)
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in starting_positions:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
 
     def move(self):
         # Made The Snake Segments to Follow Each other
-        for seg_num in range(len(segments) - 1, 0, -1):
-            new_x = segments[seg_num - 1].xcor()
-            new_y = segments[seg_num - 1].ycor()
-            segments[seg_num].goto(new_x, new_y)
-        segments[0].forward(20)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self,):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
+
