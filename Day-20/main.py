053af639b93454e9524af0a435159177e7ea0938
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 22:50:26 -0700

    Created a Segment for the snake body

--- Day-20/main.py
+++ Day-20/main.py
@@ -1,7 +1,27 @@
 from turtle import Turtle,Screen
 
-tim = Turtle()
 screen = Screen()
 
 screen.title("The Snake Game")
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+
+starting_positions = []
+
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
+
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+segment_2.goto(-20, 0)
+
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+segment_3.goto(-40, 0)
+
+
+
+
+
+
 screen.exitonclick()
