045b5bd619a123127cf424c5e5893aa40b3d1ca8
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 23:22:46 -0700

    Making the segments to follow eachother

--- Day-20/main.py
+++ Day-20/main.py
@@ -11,6 +11,9 @@ screen.tracer(0)
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
+
+# Created The snake Here
+
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
@@ -19,13 +22,12 @@ for position in starting_positions:
     segments.append(new_segment)
 
 
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
+# Made The Snake Segments to Follow Each other
     for seg_num in range(len(segments)-1, 0, -1):
         new_x = segments[seg_num - 1].xcor()
         new_y = segments[seg_num - 1].ycor()
