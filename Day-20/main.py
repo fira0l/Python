24ab0c4f14997075d487b14c667cfc39791a54d0
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-01 22:54:19 -0700

    modified the segment using the for loop

--- Day-20/main.py
+++ Day-20/main.py
@@ -8,10 +8,13 @@ screen.bgcolor("black")
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
+segments = []
+
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
 
