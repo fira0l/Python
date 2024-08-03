79108f8b71b2e842ee99f8265c4a0f403aadc8bf
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-03 06:54:19 -0700

    Used the Tracer behaviour wth update in a while loop

--- Day-22/Paddel.py
+++ Day-22/Paddel.py
@@ -1,22 +1,23 @@
 from turtle import Turtle
 
 
-X_POSITION = -435
+X_POSITION = -385
 Y_POSITION = 0
 
 
 class Paddle(Turtle):
     def __init__(self):
         super().__init__(shape="square")
+        self.shapesize(stretch_wid=5,stretch_len=1)
         self.color("white")
         self.segments = []
         self.penup()
-        self.goto(X_POSITION,Y_POSITION)
+        self.goto(X_POSITION, Y_POSITION)
 
     def up(self):
-        self.setheading(90)
-        self.forward(10)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def down(self):
-        self.setheading(270)
-        self.forward(10)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
--- Day-22/main.py
+++ Day-22/main.py
@@ -7,6 +7,7 @@ screen = Screen()
 screen.title("The Pong Game")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 
 paddle = Paddle()
 
@@ -14,5 +15,9 @@ screen.listen()
 screen.onkey(key="Up", fun=paddle.up)
 screen.onkey(key="Down", fun=paddle.down)
 
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
