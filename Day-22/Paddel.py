957bed69a2a3714b81615b6f3343a7f68179de98
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-03 08:32:42 -0700

    Made Both the Paddles movable

--- Day-22/Paddel.py
+++ Day-22/Paddel.py
@@ -1,18 +1,17 @@
 from turtle import Turtle
-
-
-X_POSITION = -385
-Y_POSITION = 0
+from typing import Tuple
 
 
 class Paddle(Turtle):
-    def __init__(self):
+
+    def __init__(self, X_POSITION, Y_POSITION):
+        self.paddle_side = (X_POSITION,Y_POSITION)
         super().__init__(shape="square")
         self.shapesize(stretch_wid=5,stretch_len=1)
         self.color("white")
         self.segments = []
         self.penup()
-        self.goto(X_POSITION, Y_POSITION)
+        self.goto(self.paddle_side)
 
     def up(self):
         new_y = self.ycor() + 20
--- Day-22/main.py
+++ Day-22/main.py
@@ -9,11 +9,14 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
-paddle = Paddle()
+left_paddle = Paddle(-385, 0)
+right_paddle = Paddle(385,0)
 
 screen.listen()
-screen.onkey(key="Up", fun=paddle.up)
-screen.onkey(key="Down", fun=paddle.down)
+screen.onkey(key="Up", fun=right_paddle.up)
+screen.onkey(key="Down", fun=right_paddle.down)
+screen.onkey(key="w", fun=left_paddle.up)
+screen.onkey(key="s", fun=left_paddle.down)
 
 game_is_on = True
 
