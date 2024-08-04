547a1375ee86aac3d2a94c6d36bb7d2a1f3a3d84
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-04 02:27:16 -0700

    just the what to do

--- Day-23/main.py
+++ Day-23/main.py
@@ -4,9 +4,15 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
+# Create Screen
 screen = Screen()
+screen.title("The Turtle Crossing Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+# Create The Turtle
+player = Player()
 
 game_is_on = True
 while game_is_on:
@@ -14,11 +20,10 @@ while game_is_on:
     screen.update()
 
 
-# Create Screen
-# Create The Turtle
 # Move The Turtle across the screen
 # create Cars
 # Move Cars along the Screen
 # Detect Collision with cars
 # Detect When The Turtle passes the End-line
 # speed-up the cars every success-full cross
+# Manage the score
--- Day-23/player.py
+++ Day-23/player.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+# Create The Turtle
+class Player(Turtle):
+    def __init__(self):
+        super().__init__(shape="turtle")
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def move_down(self):
+        self.backward(MOVE_DISTANCE)
+
+
