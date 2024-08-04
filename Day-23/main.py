cd3748889759684e55d66aaa27f850af05b8ac12
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-04 02:16:20 -0700

    Initialized the capstone project

---
+++ Day-23/main.py
@@ -0,0 +1,23 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+# Create Screen
+# Create The Turtle
+# Move The Turtle across the screen
+# create Cars
+# Move Cars along the Screen
+# Detect Collision with cars
+# Detect When The Turtle passes the End-line
