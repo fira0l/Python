80b3e95d5167b487d13b14117e2d855f5a718248
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-04 04:22:16 -0700

    Created the Score Board and it methods and Modified the player.py with adding additional methods and attributes

--- Day-23/player.py
+++ Day-23/player.py
@@ -11,12 +11,21 @@ class Player(Turtle):
         super().__init__(shape="turtle")
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.penup()
+        self.go_to_start()
         self.setheading(90)
-        self.goto(STARTING_POSITION)
 
     # Move The Turtle across the screen
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
     def move_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
 
 
