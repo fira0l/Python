615fcef40268a747e17e1c11688026ef7401c24e
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-06 16:35:08 -0700

    installed pandas package

--- Day-25/main.py
+++ Day-25/main.py
@@ -1,16 +1,16 @@
+# # with open("./weather-data.csv") as data_file:
+# #     data = data_file.readlines()
+# #     print(data)
+#
+# import csv
 # with open("./weather-data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-import csv
-with open("./weather-data.csv") as data_file:
-    data = csv.reader(data_file)
-    tempratures = []
-    for row in data:
-        if row[1] != "temp":
-            tempratures.append(int(row[1]))
-
-print(tempratures)
+#     data = csv.reader(data_file)
+#     tempratures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             tempratures.append(int(row[1]))
+#
+# print(tempratures)
 
 import pandas
 
