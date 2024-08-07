c8f90b832454b5a4896ec1ccfcc8cf33fb5be1da
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-07 05:59:58 -0700

    extracting data from csv files and manipulate it

--- Day-25/main.py
+++ Day-25/main.py
@@ -5,16 +5,34 @@
 # import csv
 # with open("./weather-data.csv") as data_file:
 #     data = csv.reader(data_file)
-#     tempratures = []
+#     temperatures = []
 #     for row in data:
 #         if row[1] != "temp":
-#             tempratures.append(int(row[1]))
+#             temperatures.append(int(row[1]))
 #
-# print(tempratures)
+# print(temperatures)
 
 import pandas
 
 data = pandas.read_csv("weather-data.csv")
-print(data)
+# print(type(data))
 
-print(data["temp"])
+# print(type(data["temp"]))
+
+#DataFrame and Series in Pandas
+
+# Data to Dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+# Data series to list
+
+temp_list = data["temp"].to_list()
+summation = 0
+average = 0
+for item in temp_list:
+    summation += item
+    average = summation / len(temp_list)
+print(f"the Average Temp is: {average}")
+print(temp_list)
+
