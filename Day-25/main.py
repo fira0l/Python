09b11f639100b2e2fc19ee0ccbadf4aa03b39d52
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-07 11:06:00 -0700

    Working on Csv Data using Pandas

--- Day-25/main.py
+++ Day-25/main.py
@@ -14,7 +14,7 @@
 
 import pandas
 
-data = pandas.read_csv("weather-data.csv")
+# data = pandas.read_csv("weather-data.csv")
 # print(type(data))
 
 # print(type(data["temp"]))
@@ -22,17 +22,43 @@ data = pandas.read_csv("weather-data.csv")
 #DataFrame and Series in Pandas
 
 # Data to Dictionary
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
 
 # Data series to list
+#
+# temp_list = data["temp"].to_list()
+# summation = 0
+# average = 0
+# for item in temp_list:
+#     summation += item
+#     average = summation / len(temp_list)
+# print(f"the Average Temp is: {average}")
+# print(temp_list)
+#
+# print(data["temp"].max())
+#
+# # Get data in columns
+# print(data["condition"])
+# print(data.condition)
 
-temp_list = data["temp"].to_list()
-summation = 0
-average = 0
-for item in temp_list:
-    summation += item
-    average = summation / len(temp_list)
-print(f"the Average Temp is: {average}")
-print(temp_list)
+#
+# print(data[data.day == "Monday"])
 
+# print(data[data.temp == data["temp"].max()])
+#
+
+# monday = data[data.day == "Monday"]
+# celcius = (monday.temp * 9/5)+32
+#
+# print(celcius)
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+print(data)
+
+data.to_csv("new_data.csv")
