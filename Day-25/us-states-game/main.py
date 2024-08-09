a594c76a911266eb2e9a98289f7f24ee50c17847
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-09 10:24:52 -0700

    Final USA STATES GUESSING game

--- Day-25/us-states-game/main.py
+++ Day-25/us-states-game/main.py
@@ -21,6 +21,12 @@ while len(guessed_states) < 50:
     all_state = data.state.to_list()
 
     if answer_state == "Exit":
+        missing_states = []
+        for state in all_state:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_state:
         guessed_states.append(answer_state)
@@ -35,12 +41,12 @@ while len(guessed_states) < 50:
 # xcor = users_choice["x"]
 # ycor = users_choice["y"]
 
-item_to_write = ""
-for item in all_state:
-    if item not in guessed_states:
-        item_to_write = item_to_write + "\n" + item
-        with open("states_to_learn", mode="w") as writee:
-            writee.write(item_to_write)
+# item_to_write = ""
+# for item in all_state:
+#     if item not in guessed_states:
+#         item_to_write = item_to_write + "\n" + item
+#         with open("states_to_learn", mode="w") as writee:
+#             writee.write(item_to_write)
 
 
 # print(xcor,ycor)
