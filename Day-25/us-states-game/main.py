fcb1e9dfb3ebf0873281ab3f18ead0305deea86f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-09 09:41:07 -0700

    Finished the first states game

--- Day-25/us-states-game/main.py
+++ Day-25/us-states-game/main.py
@@ -13,8 +13,25 @@ data = pandas.read_csv("50_states.csv")
 
 # print(data)
 
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess A State", prompt="What's another State's name?").title()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another State's name?").title()
 
+    all_state = data.state.to_list()
 
+    if answer_state in all_state:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# users_choice = data[data.state == answer_state]
+# xcor = users_choice["x"]
+# ycor = users_choice["y"]
+
+# print(xcor,ycor)
 screen.exitonclick()
