54d2d90fd14fce9d1558313259d4d0430c11d19f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-10 10:24:25 -0700

    Made Changes to the Usa State Guessing game

--- Day-25/us-states-game/main.py
+++ Day-25/us-states-game/main.py
@@ -21,10 +21,10 @@ while len(guessed_states) < 50:
     all_state = data.state.to_list()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_state:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_state if state not in guessed_states]
+        # for state in all_state:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
