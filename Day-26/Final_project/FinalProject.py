3a63b9d89a810241cfd888a9409d6fca433b7707
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-10 11:49:36 -0700

    read the key and code from file and changed it to dictionary using dictionary comprehension

---
+++ Day-26/Final_project/FinalProject.py
@@ -0,0 +1,36 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_data_dict = {row.letter: row.code for (index,row) in nato_data.iterrows()}
+print(nato_data_dict)
+# for (index,row) in
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a Word: ")
+
+nato_phonetic_converted = [nato_data_dict[letter.upper()] for letter in user_input]
+print(nato_phonetic_converted)
