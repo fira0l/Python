8e5a06b17ec97769b4db8ec9b323b17e245d0c8a
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-10 09:29:49 -0700

    List Comprehension challenge 2

--- Day-26/main.py
+++ Day-26/main.py
@@ -5,3 +5,11 @@ print(new_list)
 
 new_list = [number * 2 for number in range(1, 5)]
 print(new_list)
+
+
+# Conditional List Comprehension
+# new_list = [new_item for item in list if test]
+names = ["Alex", "Beth", "Firaol", "Dave", "Eleanor", "Freda"]
+
+short_Names = [name for name in names if len(name) < 5]
+print(short_Names)
