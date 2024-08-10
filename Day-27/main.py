95f8fad78f791c8d004e280d717243d9665b1f5b
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-10 12:27:55 -0700

    Started Developing GUI Using TKinter python module

---
+++ Day-27/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+
+window.title("My First GUI Program")
+window.minsize(width=500,height=300)
+
+# Label
+
+my_label = tkinter.Label(text="I am A Label", font=("Ariel", 15, "italic"))
+my_label.pack()
+
+
+
+window.mainloop()
+
