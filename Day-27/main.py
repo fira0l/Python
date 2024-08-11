ed0e5f596855a9ceac27976148c6350227d898ce
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-11 07:48:57 -0700

    Added an Input to the GUI using Entry

--- Day-27/main.py
+++ Day-27/main.py
@@ -7,17 +7,24 @@ window.minsize(width=500,height=300)
 
 # Label
 
-my_label = Label(text="I am A Label", font=("Ariel", 15, "italic"))
+my_label = Label(text="I am A Label", font=("Ariel", 25, "bold"))
 my_label.pack()
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
 
 def button_clicked():
-    print("I got clicked")
+    inputed_text = input.get()
+    my_label.config(text=f"{inputed_text}!!!")
 
 button = Button(text="Click Me", command=button_clicked)
 button.pack()
+
+#Entry: it is input
+
+input = Entry()
+input.pack()
+
 
 
 
