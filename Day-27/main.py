b49a7619720cad622ff607cd6100ee280096a980
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-11 07:36:43 -0700

    Created a Button To Listen Users click and prints to console

--- Day-27/main.py
+++ Day-27/main.py
@@ -1,14 +1,23 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My First GUI Program")
 window.minsize(width=500,height=300)
 
 # Label
 
-my_label = tkinter.Label(text="I am A Label", font=("Ariel", 15, "italic"))
+my_label = Label(text="I am A Label", font=("Ariel", 15, "italic"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+def button_clicked():
+    print("I got clicked")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 
