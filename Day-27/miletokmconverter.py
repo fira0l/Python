2563b244c0c067eb95f92168165a297810305184
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-12 03:45:05 -0700

    Created a Label for The app

--- Day-27/miletokmconverter.py
+++ Day-27/miletokmconverter.py
@@ -3,11 +3,17 @@ from tkinter import *
 
 window = Tk()
 window.title("Distance Converter")
-window.config(width=500,height=300)
+window.minsize(width=500,height=300)
 
 
+# Label
+label = Label()
+label.config()
+label.grid(column=0,row=0)
+# label.pack(side="left")
 
-
+# my_label = Label(text="Is Equal To")
+# my_label.grid(column=0,row=150)
 
 
 window.mainloop()
