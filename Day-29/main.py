c74e8e7fedf6e28535876e7d5c8b43f5f87077ad
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-13 23:16:08 -0700

    Created Labels for each input required

--- Day-29/main.py
+++ Day-29/main.py
@@ -14,7 +14,15 @@ lock_logo = PhotoImage(file="logo.png")
 canvas = Canvas()
 canvas.config(width=200, height=200)
 canvas.create_image(100, 100, image=lock_logo)
-canvas.pack()
+canvas.grid(column=1,row=0)
 
 
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 window.mainloop()
