88512fdbd2fd9020081c9bef64f0816dff3232c2
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-14 00:22:46 -0700

    Created a Function to handle the values Inserted into the app and write them to a file called Data.txt

--- Day-29/main.py
+++ Day-29/main.py
@@ -4,6 +4,22 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save():
+    website = website_input.get()
+    email_username = email_input.get()
+    password = password_input.get()
+
+    with open("data.txt", mode="a") as data:
+        data.writelines(f"{website} | {email_username} | {password}\n")
+
+    website_input.delete(0, END)
+    email_input.delete(0, END)
+    password_input.delete(0, END)
+
+    email_input.insert(0, "example@gmail.com")
+    # print(website, email_username, password)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -14,7 +30,7 @@ lock_logo = PhotoImage(file="logo.png")
 canvas = Canvas()
 canvas.config(width=200, height=200)
 canvas.create_image(100, 100, image=lock_logo)
-canvas.grid(column=1,row=0)
+canvas.grid(column=1, row=0)
 
 
 # Label
@@ -33,10 +49,12 @@ password_label.grid(column=0, row=3)
 website_input = Entry()
 website_input.config(width=35)
 website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
 
 email_input = Entry()
 email_input.config(width=35)
 email_input.grid(column=1, row=2, columnspan=2)
+email_input.insert(0, "example@gmail.com")
 
 password_input = Entry(width=21)
 # password_input.config()
@@ -48,7 +66,7 @@ generate_button = Button(text="Generate Password")
 # generate_button.config(width=14)
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add")
+add_button = Button(text="Add", command=save)
 add_button.config(width=36)
 add_button.grid(column=1, row=4, columnspan=2)
 
