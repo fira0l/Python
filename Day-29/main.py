cf8ba697409c1e0b4584467b6e579e5fe31851a7
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-13 23:05:12 -0700

    Created a Window and a canvas for the image(Logo)

--- Day-29/main.py
+++ Day-29/main.py
@@ -8,11 +8,12 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Generator")
+window.config(padx=20, pady=20)
 
 lock_logo = PhotoImage(file="logo.png")
 canvas = Canvas()
-canvas.config(width=200,height=200)
-canvas.create_image(100, 100,image=lock_logo)
+canvas.config(width=200, height=200)
+canvas.create_image(100, 100, image=lock_logo)
 canvas.pack()
 
 
