e67d6946d0ff0bfc1214a452fa22956f2d04fd9f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-13 23:35:03 -0700

    Created the entry and buttons for the gui

--- Day-29/main.py
+++ Day-29/main.py
@@ -17,6 +17,8 @@ canvas.create_image(100, 100, image=lock_logo)
 canvas.grid(column=1,row=0)
 
 
+# Label
+
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
@@ -25,4 +27,30 @@ email_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
+
+# Input
+
+website_input = Entry()
+website_input.config(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_input = Entry()
+email_input.config(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_input = Entry()
+password_input.config(width=21)
+password_input.grid(column=1, row=3)
+
+# Button
+
+generate_button = Button(text="Generate Password")
+generate_button.config(width=14)
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add")
+add_button.config(width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 window.mainloop()
