22ef3a7680600921c8f9c401dfcad8dd3f40d3af
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-15 02:57:58 -0700

    Some Defn of Exceptions might occur in our code and the keywords that are used to solve those exceptions

---
+++ Day-30/main.py
@@ -0,0 +1,29 @@
+## FileNotFound
+
+# with open("a_file.txt") as file:
+#     file.read()
+
+#
+## KeyError
+
+# a_dictionary = {"key":"value"}
+# value = a_dicitonary["non_existent_key"]
+
+## IndexError
+
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+## TypeError
+
+# text = "abc"
+# print(text + 5)
+
+"""
+try: Something that might cause an exception
+except: do this if there was an exception
+else: do this if there were no exceptions
+finally: Do this no matter what happens
+"""
+
+
