3799772141f2dc4a93bffc2ce90fb133b0773521
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-16 01:54:04 -0700

    Correcting a little bit of code on the nato phonetic day 26

--- Day-26/Final_project/FinalProject.py
+++ Day-26/Final_project/FinalProject.py
@@ -20,17 +20,23 @@ import pandas
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
 nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_data_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 print(nato_data_dict)
+
 # for (index,row) in
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a Word: ")
 
-nato_phonetic_converted = [nato_data_dict[letter.upper()] for letter in user_input]
-print(nato_phonetic_converted)
+def generate_phonetic():
+    user_input = input("Enter a Word: ")
+    try:
+        nato_phonetic_converted = [nato_data_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, ONly letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_phonetic_converted)
+
+generate_phonetic()
