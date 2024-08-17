7f817f877ab6cbf2312a3006b20f071764f5b543
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-17 09:29:01 -0700

    Created the Functionality of the flip card

--- Day-31/main.py
+++ Day-31/main.py
@@ -8,22 +8,32 @@ data = pd.read_csv("data/french_words.csv")
 # print(data)
 to_learn = data.to_dict(orient="records")
 # print(to_learn)
+current_card={}
 
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=current_card["French"])
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
 window.title("LangCard")
 window.config(padx=50,pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263,text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR)
