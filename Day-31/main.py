6aa78b92c9414de27c45e430e7d2eee630cf7aed
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-17 09:35:27 -0700

    Created the Functionality of the flip card

--- Day-31/main.py
+++ Day-31/main.py
@@ -12,10 +12,13 @@ current_card={}
 
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 def flip_card():
@@ -26,9 +29,9 @@ def flip_card():
 
 window = Tk()
 window.title("LangCard")
-window.config(padx=50,pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
