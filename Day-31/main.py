42340ad904e900318e1095a73566ae75e6f0ac7f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-17 08:56:42 -0700

    managed to print words randomly from the csv to the card

--- Day-31/main.py
+++ Day-31/main.py
@@ -1,7 +1,20 @@
 from tkinter import *
-
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pd.read_csv("data/french_words.csv")
+# print(data)
+to_learn = data.to_dict(orient="records")
+# print(to_learn)
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 window = Tk()
 window.title("LangCard")
@@ -11,21 +24,21 @@ window.config(padx=50,pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263,text="Word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263,text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-check_button = Button(image=check_image, highlightthickness=0)
+check_button = Button(image=check_image, highlightthickness=0, command=next_card)
 check_button.grid(row=1, column=1)
 
 
-
+next_card()
 
 window.mainloop()
 
