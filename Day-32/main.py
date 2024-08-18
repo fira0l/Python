7b303122bb90f17d5468a327ca91360514a99844
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-18 07:24:58 -0700

    Working with dates in python using datetime module

--- Day-32/main.py
+++ Day-32/main.py
@@ -1,13 +1,26 @@
-from smtplib import *
+# from smtplib import *
+#
+# my_email = "firaforpython@gmail.com"
+# password = "YourPassword"
+#
+# with SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="firaolanbessa170@gmail.com",
+#         msg="Subject:Trial Test for the python program\n\n Hello, This is a body of the email"
+#     )
 
-my_email = "firaforpython@gmail.com"
-password = "YourPassword"
 
-with SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="firaolanbessa170@gmail.com",
-        msg="Subject:Trial Test for the python program\n\n Hello, This is a body of the email"
-    )
+import datetime as dt
+
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_the_week = now.weekday()
+print(day_of_the_week)
+
+date_of_birth = dt.datetime(year=2001, month=10, day=23)
+print(date_of_birth)
