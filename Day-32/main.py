a00336859d4516731a7835117e7c7a8accc31895
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-18 09:03:37 -0700

    Working with dates in python using datetime module

--- Day-32/main.py
+++ Day-32/main.py
@@ -1,26 +1,39 @@
-# from smtplib import *
-#
-# my_email = "firaforpython@gmail.com"
-# password = "YourPassword"
-#
-# with SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="firaolanbessa170@gmail.com",
-#         msg="Subject:Trial Test for the python program\n\n Hello, This is a body of the email"
-#     )
-
-
 import datetime as dt
+import random
+from smtplib import *
 
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_the_week = now.weekday()
-print(day_of_the_week)
+weekday = now.weekday()
 
-date_of_birth = dt.datetime(year=2001, month=10, day=23)
-print(date_of_birth)
+my_email = "firaforpython@gmail.com"
+password = "odsfplyzjibggmof"
+
+if weekday == 6:
+    with open("quotes.txt", mode="r") as data_file:
+        quote = data_file.readlines()
+        quote_of_the_day = random.choice(quote)
+
+    print(quote_of_the_day)
+
+    with SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="firaolanbessa170@gmail.com",
+            msg=f"Subject:Quote Of The Day\n\n {quote_of_the_day}"
+        )
+
+
+# import datetime as dt
+#
+#
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_the_week = now.weekday()
+# print(day_of_the_week)
+#
+# date_of_birth = dt.datetime(year=2001, month=10, day=23)
+# print(date_of_birth)
