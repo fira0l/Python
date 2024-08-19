fd135d34eeb05c5a450e06e95c19627675da3d9f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-19 08:41:24 -0700

    API's With parameter

--- Day-33/main.py
+++ Day-33/main.py
@@ -1,36 +1,41 @@
 import requests
 from datetime import datetime as dt
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# data = response.json()
-#
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
+MY_LAT = 9.145000
+MY_LONG = 40.489674
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(data["iss_position"]["latitude"])
 #
 # iss_position = (longitude, latitude)
 #
 # print(iss_position)
 
-MY_LAT = 9.145000
-MY_LONG = 40.489674
-
 parameters = {
     "lat": MY_LAT,
-    "lng": MY_LONG
+    "lng": MY_LONG,
+    "formatted": 0,
 }
-
 
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+# print(data)
 
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+print(sunrise)
+print(sunset)
 
 time_now = dt.now()
-print(time_now)
+# print(time_now.hour)
+
+
 
 
