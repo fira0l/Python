d241c040aba71e96aa0b265d8e715bb353b192b4
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-19 02:10:18 -0700

    API's With parameter

--- Day-33/main.py
+++ Day-33/main.py
@@ -1,13 +1,27 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+MY_LAT = 9.145000
+MY_LONG = 40.489674
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG
+}
+
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude, latitude)
-
-print(iss_position)
+print(data)
