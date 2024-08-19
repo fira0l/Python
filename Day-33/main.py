313d1c51a797d0969213b5b94276251376cbdebc
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-19 08:47:10 -0700

    Created the is_iss_overhead and is_night Function

--- Day-33/main.py
+++ Day-33/main.py
@@ -4,37 +4,47 @@ from datetime import datetime as dt
 MY_LAT = 9.145000
 MY_LONG = 40.489674
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
 
-data = response.json()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
 
-iss_longitude = float(data["iss_position"]["longitude"])
-iss_latitude = float(data["iss_position"]["latitude"])
-#
-# iss_position = (longitude, latitude)
-#
-# print(iss_position)
+    data = response.json()
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    #
+    # iss_position = (longitude, latitude)
+    #
+    # print(iss_position)
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-# print(data)
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
 
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-print(sunrise)
-print(sunset)
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-time_now = dt.now()
-# print(time_now.hour)
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    # print(data)
+
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = dt.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+
+
 
 
 
