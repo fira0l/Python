5f3967c251146cb1711d7b2acc5611c532ca2802
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-22 08:02:00 -0700

    Added Twillio to send a message to my phone

--- Day-35/main.py
+++ Day-35/main.py
@@ -1,6 +1,10 @@
 import requests
+from twilio.rest import Client
+
 
 api_key = "d45afe4162aeb8a45ea4fd69edbce754"
+account_sid = "AC9ae698970df25fc743528d83a2cc99df"
+auth_token = "ca9b0d7c39339b9a000231787f978704"
 
 parameters = {
     "lat": 7.864380,
@@ -12,4 +16,24 @@ parameters = {
 response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+weather_slice = data["hourly"][:12]
+# print(data["hourly"][0]["weather"][0]["id"])
+# print(weather_slice)
+
+will_rain = False
+
+for hour in weather_slice:
+    condition_code = int(hour["weather"][0]["id"])
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body ="It's is Going to rain today. Remeber to bring an ☔",
+        from_="+12088048774",
+        to ="+251976104860"
+    )
+    print(message.status)
+
