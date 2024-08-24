ddc5768c361cbe66b3ac8a4cba9b110a656ee75c
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-24 03:32:55 -0700

    Code

--- Day-36/main.py
+++ Day-36/main.py
@@ -1,11 +1,14 @@
 import requests
 
 STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
 
 Alpha_API = "V5LLPZGZBLDLUCEA"
+NEWS_API_KEY = "fa42fba871b2432587b027fc44a5f8b4"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -32,5 +35,9 @@ print(difference)
 
 diff_percent = (difference / float(yesterday_closing_data)) * 100
 print(diff_percent)
+
+if diff_percent > 5:
+    print("Get News")
+
 
 
