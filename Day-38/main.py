1684ed9cd2b17201bc2b9c66d1c6d3bab083cd1f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-08-25 06:48:19 -0700

    managed to request and fetch a response from nutritionix

--- Day-38/main.py
+++ Day-38/main.py
@@ -2,9 +2,10 @@ import requests
 
 
 USER_SID = "105f4e75"
-API_KEY = "f37315f4d36bc5d593199639e2a31426"
+NUT_API_KEY = "f37315f4d36bc5d593199639e2a31426"
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
 ENDPOINT = "/v2/natural/exercise"
+GOOGLE_SHEETS_URL = "https://api.sheety.co/adacce6626dffa15bf442c3d9acb17c4/workoutsData/workouts"
 
 
 WEIGHT = 87
@@ -15,7 +16,7 @@ user_query = input("What Kind of Exercise did u perform? ")
 
 headers = {
     "x-app-id": USER_SID,
-    "x-app-key": API_KEY
+    "x-app-key": NUT_API_KEY
 }
 
 parameters = {
@@ -25,7 +26,10 @@ parameters = {
     "age": AGE
 }
 
+
 response = requests.post(url=f"{HOST_DOMAIN}{ENDPOINT}", headers=headers, json=parameters)
 response.raise_for_status()
-result = response.json()
+result = response.json()["exercises"][0]["name"]
 print(result)
+
+
