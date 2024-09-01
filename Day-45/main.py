f724e28f33caa4a5bc1e23d355ab8d5cc47a06b5
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-01 10:52:56 -0700

    Using Beautifulsoup module to scrap an html

--- Day-45/main.py
+++ Day-45/main.py
@@ -1,19 +1,42 @@
 from bs4 import BeautifulSoup
-import lxml
 
-with open("website.html", mode="r") as file:
-    content = file.read()
+import requests
 
-soup = BeautifulSoup(content, 'html.parser')
-# print(soup.p)
-all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
 
-for tag in all_anchor_tags:
-    print(tag.get("href"))
+soup = BeautifulSoup(yc_web_page, "html.parser")
 
-heading = soup.find(name="h3", class_="heading")
-print(heading.get("class"))
+titles = soup.find(name="span", class_="titleline")
+# print(titles.getText())
 
-company_url = soup.select_one(selector="p a")
-print(company_url)
+article_link = soup.find_all(name="a")
+
+
+
+
+
+
+
+
+
+
+# import lxml
+#
+# with open("website.html", mode="r") as file:
+#     content = file.read()
+#
+# soup = BeautifulSoup(content, 'html.parser')
+# # print(soup.p)
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h3", class_="heading")
+# print(heading.get("class"))
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+
