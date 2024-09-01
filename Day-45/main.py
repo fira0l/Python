5e3104323e3217b3935c63c843a91884b22b3a52
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-01 09:28:43 -0700

    Using Beautifulsoup module to scrap an html

--- Day-45/main.py
+++ Day-45/main.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
 import lxml
 
-with open("website.html", mode="r") as website:
-    content = website.read()
+with open("website.html", mode="r") as file:
+    content = file.read()
 
-soup = BeautifulSoup(content, 'lxml')
-print(soup.title)
+soup = BeautifulSoup(content, 'html.parser')
+# print(soup.p)
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))
+
+heading = soup.find(name="h3", class_="heading")
+print(heading.get("class"))
