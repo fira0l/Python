a93a72d93a10700841b3161928b7d70678d6d265
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-03 19:36:52 -0700

    Installed selenium

---
+++ Day-48/main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+# chrome_driver = "C:\chromedriver-win64\chromedriver-win64\\chromedriver.exe"
+
+driver = webdriver.Chrome()
+
+driver.get("https://github.com/fira0l")
