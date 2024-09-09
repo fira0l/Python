ac04adf1904fd2b226dbf793f597522364897f17
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-09 13:01:10 -0700

    Trying to get know selenium

--- Day-48/main.py
+++ Day-48/main.py
@@ -5,8 +5,8 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.amazon.com/Instant-Pot-Duo-Mini-Programmable/dp/B06Y1YD5W7/ref=sr_1_1?dib=eyJ2IjoiMSJ9.lTUWuLpdsGirrUITOoJeYI8El8vkTS4dSbMbAhSM0nSKAOYMJJhunBr6WhqvXg0qYpWnvRFoboq_L96rXXzLXsKsGz2Lzlkrm03S5NsO110BVZLh2hT4DGD7W3cFrm1KCT_UQMxWxzPkuuVRly10Itwc1MTbQCyLU3Et4BwvevfN4EFbCtRRT-aRU81WjeIbta_LDEOA80lvBMhkSgR5GvlnasReyVhJW-qx9DDD8jk.9pcabvRLvygrkWdK7b4ry1cbxsVvloPRxzmo6BOxcME&dib_tag=se&keywords=instant+pot&qid=1725909477&sr=8-1")
-price = driver.find_element(by="class_name", value="a-price")
-print(price)
+driver.get("https://fast.com/")
+price = driver.find_element(by='xpath', value='//*[@id="your-speed-message"]')
+print(price.tag_name)
 
 # driver.quit()
