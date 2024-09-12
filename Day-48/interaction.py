d7ef7f9e2c7e2f6e1dfd6d7ea0fd070332754207
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-12 08:54:54 -0700

    Using selenium to scrap a webpage

--- Day-48/interaction.py
+++ Day-48/interaction.py
@@ -9,7 +9,7 @@ driver.get(URL)
 article_count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 article_count.click()
 
-all_portals = driver.find_element(By.LINK_TEXT, "All Portals")
+all_portals = driver.find_element(By.LINK_TEXT, "Nominate an article")
 all_portals.click()
 
 # driver.close()
