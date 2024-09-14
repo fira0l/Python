75b7da5112cf4144f64cdb663c85efd339f8cca3
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-14 04:48:30 -0700

    Managed to get the gamesite

--- Day-48/AutomaticCookieClicker/main.py
+++ Day-48/AutomaticCookieClicker/main.py
@@ -5,3 +5,7 @@ from selenium.webdriver.common.keys import Keys
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
+
+URL = "https://orteil.dashnet.org/experiments/cookie/"
+
+driver.get(URL)
