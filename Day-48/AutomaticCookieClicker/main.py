4c55aea9c36de6ac0ef0ec0042f4a3d043e5c983
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-14 04:45:46 -0700

    Made the automated page to not close by itself

--- Day-48/AutomaticCookieClicker/main.py
+++ Day-48/AutomaticCookieClicker/main.py
@@ -2,3 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
