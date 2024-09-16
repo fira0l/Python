e50dfabea1de9faa94fcc962e40b6fd1176a3284
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-16 06:23:38 -0700

    facebook

--- Day-50/main.py
+++ Day-50/main.py
@@ -28,9 +28,19 @@ facebooksignin = driver.find_element(By.XPATH, '//*[@id="s843769042"]/div/div/di
 facebooksignin.click()
 time.sleep(30)
 
+handles = driver.window_handles
+fb_window = handles[1]
+
+driver.switch_to.window(fb_window)
 
 email = driver.find_element(By.NAME, "email")
 email.send_keys(USERPHONE)
 password = driver.find_element(By.NAME, "pass")
 password.send_keys(USERPASS)
 password.submit()
+
+time.sleep(15)
+logginas = driver.find_element(By.XPATH, '//*[@id="mount_0_0_Lm"]/div/div/div/div/div/div/div[1]/div[3]/div/div/'
+                                         'div/div/div/div/div[2]/div/div/div[1]/div/div/div/div[1]/div/div/div/div/div')
+logginas.click()
+
