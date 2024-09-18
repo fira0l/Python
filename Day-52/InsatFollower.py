b069031beb6a01713a7bcd7f85f847e54a5773f8
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-09-18 04:40:00 -0700

    Structuring the code

--- Day-52/InsatFollower.py
+++ Day-52/InsatFollower.py
@@ -1,16 +1,42 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
+
+Username = os.environ.get("USERNAME")
+Password = os.environ.get("PASSWORD")
+URL = "https://www.instagram.com"
 
 
 class InstaFollower:
     def __init__(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome(options)
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(self.options)
 
     def login(self):
-        pass
+        self.driver.get(URL)
+        time.sleep(30)
+
+        username = self.driver.find_element(By.NAME, "username")
+        username.send_keys(Username)
+        password = self.driver.find_element(By.NAME, "password")
+        password.send_keys(Password)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(45)
+
+        button = self.driver.find_element(By.CSS_SELECTOR, "div button")
+        button.click()
+
+        time.sleep(30)
+
+        notnow = self.driver.find_element(By.XPATH,
+                                     '/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]'
+                                     '/button[2]')
+        notnow.click()
 
     def find_followers(self):
         pass
--- Day-52/main.py
+++ Day-52/main.py
@@ -1,41 +1,7 @@
-import os
-import time
+from InsatFollower import InstaFollower
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-
-
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
-
-Username = os.environ.get("USERNAME")
-Password = os.environ.get("PASSWORD")
-
-URL = "https://www.instagram.com"
-
-driver.get(URL)
-time.sleep(30)
-
-username = driver.find_element(By.NAME, "username")
-username.send_keys(Username)
-password = driver.find_element(By.NAME, "password")
-password.send_keys(Password)
-password.send_keys(Keys.ENTER)
-
-time.sleep(45)
-
-button = driver.find_element(By.CSS_SELECTOR, "div button")
-button.click()
-
-time.sleep(30)
-
-notnow = driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]'
-                                       '/button[2]')
-notnow.click()
-
-
+instaFollower = InstaFollower()
+instaFollower.login()
 
 
 
