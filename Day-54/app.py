3267fa919dee43cde573cf0127ae974661d3063b
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-03 08:40:26 -0700

    Defining a variable in links

--- Day-54/app.py
+++ Day-54/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
-@app.route('/<name>')
-def greet(name):
-    return f"Hello {name}!"
+@app.route('/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello {name} you are {number} years old!"
 
 
 if __name__ == "__main__":
