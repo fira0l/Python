7be5264f8b4ce6d639c65fda539c12b07d14a53f
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-05 01:25:46 -0700

    used an api to dynamically change based on the users request

--- Day-57/server.py
+++ Day-57/server.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template
 from _datetime import datetime
+import requests
+
+AGIFY_URL = "https://api.agify.io"
+GENDERIZE_URL = "https://api.genderize.io"
 
 app = Flask(__name__)
 
@@ -13,7 +17,18 @@ def home():
 
 @app.route('/guess/<name>')
 def guess(name):
-
+    params = {
+        "name": name
+    }
+    response = requests.get(url=AGIFY_URL, params=params)
+    user_data = response.json()
+    gen_response = requests.get(url=GENDERIZE_URL, params=params)
+    user_gender = gen_response.json()
+    # print(user_data,user_gender)
+    username = name.title()
+    age = user_data["age"]
+    gender = user_gender["gender"]
+    return render_template('guess.html', user_name=username, age=age, gender=gender)
 
 
 if __name__ == "__main__":
