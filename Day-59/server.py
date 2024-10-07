1445bee1f9951f6109bb2ab0402f0b6eb583247e
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-06 21:05:22 -0700

    Managed to REquest post data and render it on homepage using npoint api

--- Day-59/server.py
+++ Day-59/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+API_URL = "https://api.npoint.io/a08e70ad31bd4e7dca38"
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    response = requests.get(url=API_URL)
+    blog_data = response.json()
+    return render_template('index.html', blog_data=blog_data)
 
 
 @app.route('/post')
