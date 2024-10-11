0d15085551c03593a87bcf75477e06afcbc85c0d
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-11 01:03:03 -0700

    Rendered the Form to add website and managed to add it to a list

--- Day-63/server.py
+++ Day-63/server.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, redirect, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "Thisismysecretkey"
+Bootstrap(app)
+
+all_books = []
+
+
+class BookSubmissionForm(FlaskForm):
+    name = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    rating = StringField("Rating")
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -8,9 +23,18 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST"])
 def add():
-    return render_template('add.html')
+    form = BookSubmissionForm()
+    if form.validate_on_submit():
+        book = {
+            "title": form.name.data,
+            "author": form.author.data,
+            "rating": form.rating.data,
+        }
+        all_books.append(book)
+
+    return render_template('add.html', form=form)
 
 
 if __name__ == '__main__':
