70c71a0e369cbee6e127b4e5b7bed0bff71b45bc
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-11 02:03:31 -0700

    Trying to work with sqlalchemy for Database

--- Day-63/server.py
+++ Day-63/server.py
@@ -3,13 +3,39 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+
+# import sqlite3
+
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# # cursor.execute(
+# #     "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(2, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Thisismysecretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 Bootstrap(app)
 
 all_books = []
 
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+    def __repr__(self):
+        return '<Title %r>' % self.title
+
+
+db.create_all()
 
 class BookSubmissionForm(FlaskForm):
     name = StringField("Title", validators=[DataRequired()])
