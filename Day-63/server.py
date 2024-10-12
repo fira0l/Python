d31b4d4b5ad6c6b328358340e0cf94a9a6eedb9d
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-12 04:48:34 -0700

    Added The Edit route and managed to change the rating of a certain book by id

--- Day-63/server.py
+++ Day-63/server.py
@@ -4,6 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from database import db, Book, App
+
 # import sqlite3
 
 # db = sqlite3.connect("books-collection.db")
@@ -18,13 +19,17 @@ app.config["SECRET_KEY"] = "Thisismysecretkey"
 
 Bootstrap(app)
 
-
 all_books = []
 
 
 class BookSubmissionForm(FlaskForm):
     name = StringField("Title", validators=[DataRequired()])
     author = StringField("Author", validators=[DataRequired()])
+    rating = StringField("Rating")
+    submit = SubmitField("Submit")
+
+
+class EditRatingForm(FlaskForm):
     rating = StringField("Rating")
     submit = SubmitField("Submit")
 
@@ -55,6 +60,24 @@ def add():
         return render_template('index.html', books=books)
 
     return render_template('add.html', form=form)
+
+
+@app.route('/edit/<int:book_id>',methods=["GET", "POST"])
+def edit(book_id):
+    edit_form = EditRatingForm()
+
+    with App.app_context():
+        books = Book.query.all()
+
+        book = Book.query.filter_by(id=book_id)
+        book_data = [book.all()]
+        book_to_update_by_filter = Book.query.filter_by(id=book_id).first()
+        if edit_form.validate_on_submit():
+            changed_rating = edit_form.rating.data
+            book_to_update_by_filter.rating = changed_rating
+            db.session.commit()
+            return redirect('/')
+    return render_template('edit.html', book=book_data, form=edit_form)
 
 
 if __name__ == '__main__':
