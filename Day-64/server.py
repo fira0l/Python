036e5bb3b85cb439fa15cdb184c2d01d72560237
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-12 09:09:40 -0700

    Created The necessary Routes to a webfile

--- Day-64/server.py
+++ Day-64/server.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from database import App, db, Movie
-
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -15,6 +14,18 @@ def home():
 
 @app.route('/add')
 def add():
+    with App.app_context():
+        new_movie = Movie(
+            title="Phone Booth",
+            year=2002,
+            description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+            rating=7.3,
+            ranking=10,
+            review="My favourite character was the caller.",
+            img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+        )
+        db.session.add(new_movie)
+        db.session.commit()
     return render_template('add.html')
 
 
@@ -29,4 +40,6 @@ def edit():
 
 
 if __name__ == "__main__":
+    with App.app_context():
+        db.create_all()
     app.run(debug=True)
