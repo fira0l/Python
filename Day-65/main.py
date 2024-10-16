9304943a382a0c80f2c9ab073e8aed8d3503fa04
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-16 01:36:23 -0700

    Modified the main file code to handle the jsonify

--- Day-65/main.py
+++ Day-65/main.py
@@ -17,17 +17,7 @@ def get_random_cafe():
         cafes = Cafe.query.all()
         print(cafes)
         random_cafe = random.choice(cafes)
-        jsondata = jsonify(cafe={"id": random_cafe.id,
-                                 "name": random_cafe.name,
-                                 "map_url": random_cafe.map_url,
-                                 "img_url": random_cafe.img_url,
-                                 "location": random_cafe.location,
-                                 "seats": random_cafe.seats,
-                                 "has_toilet": random_cafe.has_toilet,
-                                 "has_wifi": random_cafe.has_wifi,
-                                 "has_sockets": random_cafe.has_sockets,
-                                 "can_take_calls": random_cafe.can_take_calls,
-                                 "coffee_price": random_cafe.coffee_price, })
+        jsondata = jsonify(cafe=random_cafe.to_dict())
         return jsondata
 
 
