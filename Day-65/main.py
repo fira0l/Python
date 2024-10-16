1b569c7dc962843d189b86bcd486626f0c0beb2a
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-16 01:30:51 -0700

    Jsonified the return value of random get request to the database

--- Day-65/main.py
+++ Day-65/main.py
@@ -1,42 +1,34 @@
 import random
-
+from database import db, App, Cafe
 from flask import Flask, jsonify, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-##Cafe TABLE Configuration
-class Cafe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
-    map_url = db.Column(db.String(500), nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
-    has_toilet = db.Column(db.Boolean, nullable=False)
-    has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
-    can_take_calls = db.Column(db.Boolean, nullable=False)
-    coffee_price = db.Column(db.String(250), nullable=True)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
 
 ## HTTP GET - Read Record
 @app.route('/random', methods=["GET"])
 def get_random_cafe():
-    cafes = db.query(Cafe).all()
-    random_cafe = random.choice(cafes)
-
+    with App.app_context():
+        cafes = Cafe.query.all()
+        print(cafes)
+        random_cafe = random.choice(cafes)
+        jsondata = jsonify(cafe={"id": random_cafe.id,
+                                 "name": random_cafe.name,
+                                 "map_url": random_cafe.map_url,
+                                 "img_url": random_cafe.img_url,
+                                 "location": random_cafe.location,
+                                 "seats": random_cafe.seats,
+                                 "has_toilet": random_cafe.has_toilet,
+                                 "has_wifi": random_cafe.has_wifi,
+                                 "has_sockets": random_cafe.has_sockets,
+                                 "can_take_calls": random_cafe.can_take_calls,
+                                 "coffee_price": random_cafe.coffee_price, })
+        return jsondata
 
 
 ## HTTP POST - Create Record
@@ -47,4 +39,6 @@ def get_random_cafe():
 
 
 if __name__ == '__main__':
+    with App.app_context():
+        db.create_all()
     app.run(debug=True)
