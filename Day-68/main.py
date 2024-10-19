d4b7a00bbd4bd2a1d531252cdaf483fd667701d7
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-10-19 10:12:18 -0700

    Adding some Success Finishes

--- Day-68/main.py
+++ Day-68/main.py
@@ -24,7 +24,7 @@ def load_user(user_id):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -46,7 +46,7 @@ def register():
             db.session.commit()
             login_user(new_user)
             return render_template('secrets.html', name=new_user.name)
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -68,14 +68,14 @@ def login():
                 login_user(user)
                 return render_template('secrets.html', name=user.name)
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
